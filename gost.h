8979ee1a3c72a53e60686c0e33f83b157fda1e4c
Valentina-Ermolaeva-KI1102 <luna650@mail.ru>
2014-12-09 22:40:09 +0800

    Create gost.h

---
+++ gost.h
@@ -0,0 +1,9 @@
+int fi(int arg);
+void E_f(unsigned char A[], unsigned char K[], unsigned char R[]);
+void E(unsigned char D[], unsigned char K[], unsigned char R[]);
+void A(unsigned char Y[], unsigned char R[]);
+void P(unsigned char Y[], unsigned char R[]);
+void psi(unsigned char arr[]);
+void psi(unsigned char arr[], int p);
+void f(unsigned char H[], unsigned char M[], unsigned char newH[]);
+bool gost(char *inFile, char *outFile);
